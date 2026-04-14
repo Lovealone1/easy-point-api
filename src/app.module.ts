d5c5f620825d330a841a90cde1e80c4b1c5d4f1f
Dan <dgarcia232@soyudemedellin.edu.co>
2026-04-13 21:40:23 -0500

    feat: :construction: Global http logger and custom logger

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,13 @@
-import { Module } from '@nestjs/common';
+import { Module, MiddlewareConsumer, NestModule } from '@nestjs/common';
+import { LoggerMiddleware } from '@/common/middlewares/logger.middleware.js';
 
 @Module({
   imports: [],
   controllers: [],
   providers: [],
 })
-export class AppModule { }
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(LoggerMiddleware).forRoutes('*');
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module.js';
+import { AppLogger } from '@/common/logger/app.logger.js';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    bufferLogs: true,
+  });
+  app.useLogger(new AppLogger());
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
