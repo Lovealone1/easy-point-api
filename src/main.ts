3045074dea018195cd36d29282d9b08bd900079f
Dan <dgarcia232@soyudemedellin.edu.co>
2026-04-14 00:30:13 -0500

    feat: :construction: financial exceptions and money value object to avoid decimal issues

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module.js';
 import { AppLogger } from '@/common/logger/app.logger.js';
+import { GlobalExceptionFilter } from '@/common/filters/global-exception.filter.js';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     bufferLogs: true,
   });
   app.useLogger(new AppLogger());
+  app.useGlobalFilters(new GlobalExceptionFilter());
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
