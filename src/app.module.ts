99652dd07195d86a7457d8a60b35323984313f98
Dan <dgarcia232@soyudemedellin.edu.co>
2026-04-15 02:16:34 -0500

    feat: :construction: Request info middleware

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,7 @@ import { Module, MiddlewareConsumer, NestModule } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { JsonBodyMiddleware } from './common/middlewares/json-body.middleware.js';
 import { LoggerMiddleware } from './common/middlewares/logger.middleware.js';
+import { RequestInfoMiddleware } from './common/middlewares/request-info.middleware.js';
 import { RateLimitMiddleware } from './common/middlewares/rate-limit.middleware.js';
 import { RedisModule } from './infraestructure/redis/redis.module.js';
 import appConfig from './common/config/config.js';
@@ -22,7 +23,12 @@ import { MailService } from './infraestructure/mail/mail.service.js';
 export class AppModule implements NestModule {
   configure(consumer: MiddlewareConsumer) {
     consumer
-      .apply(JsonBodyMiddleware, LoggerMiddleware, RateLimitMiddleware)
+      .apply(
+        JsonBodyMiddleware,
+        RequestInfoMiddleware,
+        LoggerMiddleware,
+        RateLimitMiddleware,
+      )
       .forRoutes('{*path}');
   }
 }
